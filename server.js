2866675292a60f9c91bdd42f1b05b5a30c9d2ade
Peyton Cysewski <pcysewski12@gmail.com>
2020-04-14 13:01:43 -0700

    added fake usibility from data/geo.json

--- server.js
+++ server.js
@@ -3,11 +3,30 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
 
-// 
+// Getting a location
+app.get('/location', (request, response) => {
+
+let locData = require('./data/geo.json');
+let city = request.query.city;
+let formattedObj = new City(city, locData);
+
+response.status(200).send(formattedObj);
+});
+
+function City(city, locData) {
+  this.search_query = city;
+  this.formatted_query = locData[0].display_name;
+  this.latitude = locData[0].lat;
+  this.longitude = locData[0].lon;
+}
+
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
