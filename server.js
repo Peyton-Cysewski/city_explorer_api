6daa19be3d182fa535643a71d51fc93162fbddfb
Peyton Cysewski <pcysewski12@gmail.com>
2020-04-15 01:26:23 -0700

    added feature 3 of lab 06

--- server.js
+++ server.js
@@ -14,18 +14,39 @@ app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
 // Getting a location
 app.get('/location', (request, response) => {
 
-let locData = require('./data/geo.json');
+let locationData = require('./data/geo.json');
 let city = request.query.city;
-let formattedObj = new City(city, locData);
+let formattedObj = new City(city, locationData);
 
 response.status(200).send(formattedObj);
 });
 
+// Getting a location's Weather
+app.get('/weather', (request, response) => {
+  let empty = [];
+
+  let weatherData = require('./data/darksky.json')
+  weatherData.data.forEach(item => {
+    let forecast = item.weather.description;
+    let time = new Date(item.valid_date).toDateString();
+    let formattedObj = new Weather(forecast,time); 
+    
+    empty.push(formattedObj)
+  });
+  response.status(200).send(empty);
+  });
+
+// Constructor Functions 
 function City(city, locData) {
   this.search_query = city;
   this.formatted_query = locData[0].display_name;
   this.latitude = locData[0].lat;
   this.longitude = locData[0].lon;
+}
+
+function Weather(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
 }
 
 
